803f769151086bcb3a069cf83aadf7412d62da48
Nikhil <deadofied@live.com>
2014-04-29 19:25:32 -0400

    one_file

--- Code/melody.py
+++ Code/melody.py
@@ -1,5 +1,6 @@
 import random
-
+import lilypond
+import matplotlib.pyplot as plt
 
 VALID_INTERVALS = [2, 4, 5, 7, 9, 11] # Intervals between notes that are allowed (fourth sepcies counterpoint)
 
@@ -364,3 +365,34 @@ class Melody(object):
     def __init__(self, notes):
         self.notes = notes
         self.fitness = None # set by the fitness function.
+
+
+if __name__ == '__main__':
+    # cantus_firmus
+    cf = [5, 7, 6, 5, 8, 7, 9, 8, 7, 6, 5]
+
+    population_size = 1000
+    
+    start_population = create_population(population_size)
+
+    ga = genetic_algorithm(start_population, fitness_function, spawn_function, halt_function)
+    
+    fitness = 0.0
+    fitnesses = []
+
+    counter = 0
+    for generation in ga:
+        counter += 1
+        fitness = generation[0].fitness
+        fitnesses.append(fitness)
+        print "--- Generation %d ---" % counter
+        print generation[0].notes
+        print fitness
+
+    plt.plot(fitnesses)
+    plt.ylabel('Generation')
+    plt.xlabel('Fitness Score')
+    plt.savefig('output/fitness_test.png')
+    
+    with open('output/test.ly', 'w') as output:
+        output.write(lilypond.render(cf, generation[0].notes))
